cd9d1d0bbe99fac18ae613e2f89e165f98467494
Rockwell Rabago <rockwellrab@gmail.com>
2025-12-31 15:47:36 -0800

    Add automated EBIT pull by ticker

--- data_pull.py
+++ data_pull.py
@@ -2,7 +2,7 @@
 import yfinance as yf
 import pandas as pd
 
-#Function to get revenues from a ticker
+#  get revenues from a ticker
 
 def get_revenues(ticker):
     t_symbol = yf.Ticker(ticker)
@@ -13,7 +13,23 @@ def get_revenues(ticker):
     revenue_list = clean_revenues.to_list()
     revenue_list.reverse()
 
+    return revenue_list
 
+
+##get_revenues("AAPL")
+
+# Gets the Ebits using a ticker symbol
+def get_ebit(ticker):
+     t_symbol = yf.Ticker(ticker)
+     income_stmt = t_symbol.financials 
+
+     ebits = income_stmt.loc["EBIT"]
+     clean_ebits = ebits.dropna()
+     ebit_list = clean_ebits.to_list()
+     ebit_list.reverse()
+
+     return ebit_list
+   
 
 
     
