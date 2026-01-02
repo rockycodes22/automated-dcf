0b74a7cf61a1b1e2a7a0ea0b8b5576c2da086e17
Rockwell Rabago <rockwellrab@gmail.com>
2026-01-02 13:23:23 -0800

    Add run_dcf orchestrator and complete automated DCF pipeline

--- dcf_model.py
+++ dcf_model.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 #  get revenues from a ticker
 
-def get_revenues(ticker):
+def get_income_statement(ticker):
     t_symbol = yf.Ticker(ticker)
     income_stmt = t_symbol.financials
 
@@ -17,36 +17,25 @@ def get_revenues(ticker):
     revenue_list = clean_revenues.to_list()
     revenue_list.reverse()
 
-    return revenue_list
+    ebits = income_stmt.loc["EBIT"]
+    clean_ebits = ebits.dropna()
+    ebit_list = clean_ebits.to_list()
+    ebit_list.reverse()
+   
+    return revenue_list, ebit_list
 
-
-
-# Gets the Ebits using a ticker symbol
-def get_ebit(ticker):
-     t_symbol = yf.Ticker(ticker)
-     income_stmt = t_symbol.financials 
-
-     ebits = income_stmt.loc["EBIT"]
-     clean_ebits = ebits.dropna()
-     ebit_list = clean_ebits.to_list()
-     ebit_list.reverse()
-
-     return ebit_list
    
 # calculate ebit margin: ebit/revenue
 
-def get_ebit_margin(ticker):
-     ebit = get_ebit(ticker)
-     revenue = get_revenues(ticker)
+def get_ebit_margins(revenues, ebits):
+    ebit_margins = []
 
-     ebit_margins = []
+    for i in range(len(ebits)):
+        temp_margin = ebits[i] / revenues[i]
+        ebit_margins.append(temp_margin)
 
-     # loop to calc ebit margins
-     for i in range(len(ebit)):
-          temp_margin = ebit[i]/revenue[i]
-          ebit_margins.append(temp_margin)
+    return ebit_margins
 
-     return ebit_margins
 
 # function to calculate historical growth rates
 def get_growth_rates(revenues):
@@ -208,4 +197,67 @@ def sensitivity_table(ufcf,wacc_values,g_values):
               table.loc[wacc, g] = ev
      
      return table
+
+
+def run_dcf(ticker, assumptions):
+    # pull data
+      revenues, ebits = get_income_statement(ticker)
+    # historical analysis
+
+      ebit_margins = get_ebit_margins(revenues, ebits)
+      growth_rates = get_growth_rates(revenues)
+
+      avg_ebit_margin = average_ebit_margin(ebit_margins)
+      avg_growth_rate = average_growth(growth_rates)
+
+    # forecasting
+      forecasted_revenues = forecast_revenues(revenues[-1],avg_growth_rate,assumptions["forecast_years"])
+      forecasted_ebits = forecast_ebits(forecasted_revenues, avg_ebit_margin)
+
+      forecasted_nopat = forecast_nopat(forecasted_ebits,assumptions["tax_rate"])
+      forecasted_da = forecast_da(forecasted_revenues,assumptions["da_percentage"])
+      forecasted_capex = forecast_capex(forecasted_revenues, assumptions["capex_percentage"])
+      forecasted_nwc = forecast_nwc(forecasted_revenues, assumptions["nwc_percentage"])
+
+      ufcf = forecast_ufcf(forecasted_nopat,forecasted_da, forecasted_capex, forecasted_nwc)
+      pv_ufcf = discount_ufcf(ufcf,assumptions["wacc"])
+      
+
+    # valuation
+      pv_tv = find_terminal_value(ufcf, assumptions["wacc"], assumptions["terminal_g"])
+      ev = find_ev(pv_ufcf, pv_tv)
+      implied_share_price = find_implied_share_price(ev, assumptions["cash"], assumptions["debt"], assumptions["shares_outstanding"])
+    # sensitivity
+
+      sensitivity = sensitivity_table(ufcf, assumptions["wacc_range"], assumptions["terminal_g_range"])
+
+      results = {
+      "summary": {
+        "enterprise_value": ev,
+        "implied_share_price": implied_share_price
+      },
+ 
+      "historical": {
+        "revenues": revenues,
+        "ebits": ebits,
+        "ebit_margins": ebit_margins,
+        "growth_rates": growth_rates
+      },
+
+      "forecast": {
+        "revenues": forecasted_revenues,
+        "ebits": forecasted_ebits,
+        "nopat": forecasted_nopat,
+        "ufcf": ufcf
+      },
+
+      "valuation": {
+        "pv_ufcf": pv_ufcf,
+        "pv_terminal_value": pv_tv,
+        "sensitivity": sensitivity
+      }
+      }
+
+      return results
+
 
